76e510de9afb0ac5dc1a26e7df93b5f9496e2c35
FHGDev <gideon.lingle1@gmail.com>
2018-10-26 15:11:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,7 @@ const router = require('express').Router()
 const hook = new (require('discord.js').WebhookClient)("")
 router.post('/', (req, res, next) => {
   console.log("Hook received.")
-  if (res.headers.Authorization == process.env.dblwhtok) {
+  if (req.header("Authorization") == process.env.dblapikey) {
     // SOON
   }
-})
-
-router.listen(process.env.PORT || 5000, () => {
-  console.log("Listening on port 5000.")
 })
